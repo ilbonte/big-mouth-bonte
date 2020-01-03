d6e114a34980f938ed0026d7f205da54fe411289
Davide Bontempelli <davide.bonte@gmail.com>
2020-01-03 10:50:30 +0100

    return basic html hello world

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,16 +1,15 @@
 'use strict';
 
+const fs = require("fs")
+
 module.exports.handler = async event => {
+  let html = "<html><body><h1>hello</h1></body></html>"
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: html,
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8'
+    }
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
