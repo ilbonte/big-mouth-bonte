70d07fbe74c861f0ca51aeab1e150fab646e5904
Davide Bontempelli <davide.bonte@gmail.com>
2020-01-03 11:42:55 +0100

    read static html file instad of hardcoded html

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const fs = require("fs")
+const fs = require('fs').promises
+
+async function loadHtml() {
+  return await fs.readFile('./static/index.html', 'utf-8')
+}
 
 module.exports.handler = async event => {
-  let html = "<html><body><h1>hello</h1></body></html>"
+  let html = await loadHtml();
   return {
     statusCode: 200,
     body: html,
@@ -11,7 +15,4 @@ module.exports.handler = async event => {
       'Content-Type': 'text/html; charset=UTF-8'
     }
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
