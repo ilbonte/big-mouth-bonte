88f7ed563361754604c3f1e416a57f911ab3f3b3
Davide Bontempelli <davide.bonte@gmail.com>
2020-01-03 12:38:26 +0100

    render a static list of restaurants (using mustache template)

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const fs = require('fs').promises
+const Mustache = require('mustache');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 async function loadHtml() {
   return await fs.readFile('./static/index.html', 'utf-8')
 }
 
+
 module.exports.handler = async event => {
-  let html = await loadHtml();
+  let template = await loadHtml();
+  let restaurants = await getRestaurants()
+  let dayOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template, { dayOfWeek, restaurants });
   return {
     statusCode: 200,
     body: html,
@@ -16,3 +22,48 @@ module.exports.handler = async event => {
     }
   };
 };
+
+async function getRestaurants() {
+  return [
+    {
+      name: "Fangtasia",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png",
+      themes: ["true blood"]
+    },
+    {
+      name: "Shoney's",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/shoney's.png",
+      themes: ["cartoon", "rick and morty"]
+    },
+    {
+      name: "Freddy's BBQ Joint",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/freddy's+bbq+joint.png",
+      themes: ["netflix", "house of cards"]
+    },
+    {
+      name: "Pizza Planet",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png",
+      themes: ["netflix", "toy story"]
+    },
+    {
+      name: "Leaky Cauldron",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png",
+      themes: ["movie", "harry potter"]
+    },
+    {
+      name: "Lil' Bits",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png",
+      themes: ["cartoon", "rick and morty"]
+    },
+    {
+      name: "Fancy Eats",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png",
+      themes: ["cartoon", "rick and morty"]
+    },
+    {
+      name: "Don Cuco",
+      image: "https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png",
+      themes: ["cartoon", "rick and morty"]
+    },
+  ];
+}
