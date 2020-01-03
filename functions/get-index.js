cbe245d16828cb47442a6134138614a10c32d50a
Davide Bontempelli <davide.bonte@gmail.com>
2020-01-03 17:28:06 +0100

    secure get restaurant with iam authorization

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const fs = require('fs').promises
+const URL = require('url')
 const axios = require('axios').default;
+const aws4 = require('aws4')
 const Mustache = require('mustache');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const restaurantsApiRoot = process.env.restaurants_api;
@@ -26,8 +28,23 @@ module.exports.handler = async event => {
 };
 
 async function getRestaurants() {
+  let url = URL.parse(restaurantsApiRoot)
+  let opts = {
+    host: url.hostname,
+    path: url.pathname
+  }
+
+  aws4.sign(opts)
+
   try {
-    const response = await axios.get(restaurantsApiRoot);
+    const response = await axios.get(restaurantsApiRoot, {
+      headers: {
+        "Host": opts.headers["Host"],
+        "X-Amz-Date": opts.headers["X-Amz-Date"],
+        "Authorization": opts.headers["Authorization"],
+        "X-Amz-Security-Token": opts.headers["X-Amz-Security-Token"]
+      }
+    });
     return response.data
   } catch (error) {
     console.error(error);
